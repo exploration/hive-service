e1c848e1e21770f15a15f6a46ce07b176b135ad1
Donald Merand <dmerand@explo.org>
2017-11-18 19:49:09 -0500

    add custom type HiveAtom.t(), to further clarify the Atom spec.

--- lib/hive_atom.ex
+++ lib/hive_atom.ex
@@ -10,25 +10,36 @@ defmodule HiveAtom do
   """ 
 
 
-  ### TYPES ###
-
-  @typedoc """
-    A "triplet" is a fundamental unit of a HIVE Atom: it is the application,
-    context, and process that created the atom. For example, a atom created by
-    Portico, where a user is updated might have the triplet signature
-    `{"portico", "user", "update"}`.
-  """
-  @type triplet :: {String.t(), String.t(), String.t()}
-
-
   ### STRUCTS ###
 
   defstruct [
     :application, :context, :process,
     :data,
     :receipts,
-    :id, :created_at, :updated_at
+    :id,
+    :created_at, :updated_at
   ]
+
+  @typedoc "A Hive ATOM"
+  @type t :: %__MODULE__{
+    application: String.t(),
+    context: String.t(),
+    process: String.t(),
+    data: String.t(),
+    receipts: String.t() | nil,
+    id: integer() | nil,
+    created_at: String.t() | nil,
+    updated_at: String.t() | nil
+  }
+
+  @typedoc """
+  A "triplet" is a fundamental unit of a HIVE Atom: it is the application,
+  context, and process that created the atom.
+  
+  For example, an atom created by Portico, where a user is updated might have
+  the triplet signature `{"portico", "user", "update"}`.
+  """
+  @type triplet :: {String.t(), String.t(), String.t()}
 
 
   ### FUNCTIONS ###
@@ -38,7 +49,7 @@ defmodule HiveAtom do
 
   In the event of decode error, return an empty map.
   """
-  @spec data_map(%HiveAtom{}) :: Poison.Parser.t() | no_return() | map()
+  @spec data_map(HiveAtom.t()) :: Poison.Parser.t() | no_return() | map()
   def data_map(atom = %HiveAtom{}) do
     try do
       Poison.decode!(atom.data)
@@ -53,7 +64,7 @@ defmodule HiveAtom do
 
   Returns a `%HiveAtom{}` struct.
   """
-  @spec from_map(map()) :: %HiveAtom{}
+  @spec from_map(map()) :: HiveAtom.t()
   def from_map(atom_map) when is_map(atom_map) do
     %HiveAtom{
       application: key_variants(atom_map, :application),
@@ -75,7 +86,7 @@ defmodule HiveAtom do
 
   This function returns a triplet (see the triplet() type above)
   """
-  @spec triplet(%HiveAtom{}) :: triplet()
+  @spec triplet(HiveAtom.t()) :: triplet()
   def triplet(atom = %HiveAtom{}) do
     {atom.application, atom.context, atom.process}
   end
