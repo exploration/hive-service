49df5324301cae40af82d031d13ae09517f8066b
Sam Osborn <37220048+samosborn@users.noreply.github.com>
2022-06-13 16:52:31 -0400

    more debugging flags

--- lib/hive_service.ex
+++ lib/hive_service.ex
@@ -217,10 +217,11 @@ defmodule HiveService do
   end
 
   defp post(endpoint, body) do
+    IO.puts("at endpoint: #{endpoint}")
     endpoint
     |> HTTPoison.post!(body, headers())
     |> run_unless_auth_error(fn response ->
-      IO.puts(response.body)
+      IO.puts("got back: #{response.body}")
       response.body
       |> Jason.decode!()
       |> convert_maps_to_hiveatoms()
